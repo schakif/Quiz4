57f31881dd81df235ecbd89c539df920098f7a7d
Steve Chakif <schakif@Steves-MacBook-Pro.local>
2014-02-06 18:51:01 -0500

    Initial Commit

---
+++ Quiz4/Quiz4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz4AppDelegate.h
+//  Quiz4
+//
+//  Created by Steve Chakif on 2/6/14.
+//  Copyright (c) 2014 Steve Chakif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz4
+//
+//  Created by Steve Chakif on 2/6/14.
+//  Copyright (c) 2014 Steve Chakif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Quiz4AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Quiz4AppDelegate class]));
+    }
+}
